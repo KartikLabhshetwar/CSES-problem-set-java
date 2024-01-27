524ecd094d9b4f7edb08b5f654026d60947d535d
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2024-01-27 14:26:40 +0530

    add new file

--- Introductory Problems/missingNumber.java
+++ Introductory Problems/missingNumber.java
@@ -4,17 +4,18 @@ public class missingNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long n = sc.nextLong();
-        long[] arr = new long[(int)n-1];
+        long[] arr = new long[(int) n-1];
         int sumArr = 0;
 
        for(int i = 0; i < n-1; i++) {
-           arr[i] = sc.nextInt();
+           arr[i] = sc.nextLong();
            sumArr += arr[i];
        }
-
         long sum = (long) n * (n+1) / 2;
+
+        long result = sum - sumArr;
             
-        System.out.println(sum - sumArr);
+        System.out.println(result);
         
         sc.close();
     }
