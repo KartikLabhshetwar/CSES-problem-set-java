21161d679e15459e8db294687d65ec9dd24c2bd0
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2024-01-30 21:12:52 +0530

    bug fix

--- Introductory Problems/Repetitions.java
+++ Introductory Problems/Repetitions.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 public class Repetitions {
      public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            String s = sc.nextLine();
 
-        String s = sc.nextLine();
+            int max = 0;
+            int count = 1;
 
-        int max = 0;
-        int count = 1;
-
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i-1)) {
-                count++;
-            } else {
-                max = Math.max(max, count);
-                count = 1;
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == s.charAt(i-1)) {
+                    count++;
+                } else {
+                    max = Math.max(max, count);
+                    count = 1;
+                }
             }
-        }
 
-        max = Math.max(max, count);
-        System.out.println(max);
+            max = Math.max(max, count);
+            System.out.println(max);
+        }
      }
 }
