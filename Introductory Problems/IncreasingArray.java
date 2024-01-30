1e77d4da93a272e63ba804f99fe6c46b40c08545
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2024-01-30 21:12:20 +0530

    bug fixed

--- Introductory Problems/IncreasingArray.java
+++ Introductory Problems/IncreasingArray.java
@@ -3,30 +3,30 @@ import java.util.*;
 public class IncreasingArray {
     public static void main(String args[]){
 
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            //input size of array
+            int n = sc.nextInt();
 
-        //input size of array
-        int n = sc.nextInt();
+            //declare array
+            long[] arr = new long[n];
 
-        //declare array
-        long[] arr = new long[n];
+            //declare count variable
+            long count = 0;
 
-        //declare count variable
-        long count = 0;
-
-        //input array
-        for(int i=0;i<n;i++){
-            arr[i] = sc.nextLong();
-        }
-
-       //logic starts here
-        for(int i = 1; i < n ; i++){
-            if(arr[i]<arr[i-1]){
-                count += arr[i-1]-arr[i];
-                arr[i] = arr[i-1];
+            //input array
+            for(int i=0;i<n;i++){
+                arr[i] = sc.nextLong();
             }
-        }
 
-        System.out.println(count);
+      //logic starts here
+            for(int i = 1; i < n ; i++){
+                if(arr[i]<arr[i-1]){
+                    count += arr[i-1]-arr[i];
+                    arr[i] = arr[i-1];
+                }
+            }
+
+            System.out.println(count);
+        }
     }
 }
