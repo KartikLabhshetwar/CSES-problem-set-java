d5939cc5471fc45079acc687d6c5ebc42851ff5b
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2024-02-10 14:48:00 +0530

    fix

--- Introductory Problems/TowerOfHanoi.java
+++ Introductory Problems/TowerOfHanoi.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 public class TowerOfHanoi {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println((int) Math.pow(2, n) - 1);
-        solve(n, 1, 3, 2);
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            System.out.println((int) Math.pow(2, n) - 1);
+            solve(n, 1, 3, 2);
+        }
     }
 
     public static void solve(int n, int from, int to, int aux) {
